e574216021ca3512eebfb06af5eed33bee8a82d2
USER1995 <userfrom1995@gmail.com>
2024-06-17 23:34:31 +0530

    Added Tutorial Folder

--- Node/Event.js
+++ Node/Event.js
@@ -5,5 +5,7 @@ class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 myEmitter.on('event', () => {
   console.log('an event occurred!');
+  setTimeout(() => {
+    console.log('This code will run after the previous line');
+  }, 3000);
 });
-myEmitter.emit('event');
